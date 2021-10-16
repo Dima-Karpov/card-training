331276cf5954db095e69727bc444d0084052b8bd
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-10-16 19:13:55 +0300

    'delet-modal-window'

--- src/ui/common/ModalWindow/ModalDelete/ModalDelete.tsx
+++ src/ui/common/ModalWindow/ModalDelete/ModalDelete.tsx
@@ -14,23 +14,25 @@ export const ModalDelete: React.FC<ModalDeletPropsType> = React.memo(props => {
 
     return (
         <div className={s.modalDelet}>
-            <div className={s.modalTop}>
-                <h2 className={s.caption}>Delete {name}: </h2>
-                <button onClick={onCloseModalButtonClick} className={s.btnCross} />
+            <div className={s.captionBtn}>
+                <button onClick={onCloseModalButtonClick} className={s.btnClose} />
             </div>
-            <div>
+            <h2 className={s.caption}>Delete {name}: </h2>
+            <div className={s.inputWrap}>
                 <span className={s.text}>Do you really want to remove
-                    <span className={s.accent}>{name} Name - {packName} </span>
+                    <span className={s.accent}> {name} name - <span style={{ color: 'green' }}>{packName}</span>? </span>
                     All cards will be excluded from this course.
                 </span>
             </div>
             <div className={s.btns}>
                 <Button
+                    red={true}
                     name='Cansel'
                     onClick={onCloseModalButtonClick}
                     className={s.button}
                 />
                 <Button
+                    red={true}
                     name='Delete'
                     onClick={onDeleteButtonClick}
                     className={s.button}
