c3bb1996a7b020f1a2cafdf451d55b720349e4c8
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-11-01 18:27:03 +0300

    'style-rating'

--- src/ui/common/Rating/Ratin.tsx
+++ src/ui/common/Rating/Ratin.tsx
@@ -1,6 +1,7 @@
 import { Rating, RatingProps } from "@mui/material";
 import React from "react";
-import { useStyles } from "./RatingMUI";
+import s from './Rating.module.css'
+
 
 export const RatingMUI = React.memo((props: RatingProps) => {
 
@@ -8,9 +9,9 @@ export const RatingMUI = React.memo((props: RatingProps) => {
   // const classes = useStyles();
 
   return (
-    <div>
-      <Rating  {...props} readOnly /> 
-    </div>
+    <>
+      <Rating  {...props} readOnly className={s.rating} />
+    </>
   )
 })
 
